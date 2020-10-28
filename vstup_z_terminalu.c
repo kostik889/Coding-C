ab9e7102060723391f29d79480a0ea6098a73031
Matěj Kostrhoun <matej@LAPTOP-IS5INFVB.KOSTRHOUN.local>
2020-10-29 00:34:59 +0100

    kodovani CP 852

---
+++ vstup_z_terminalu.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    
+    int cislo;
+
+    printf("Zadejte Ÿ¡slo: ");
+    scanf("%d", &cislo);
+
+    printf("Toto Ÿ¡slo jste zadali: %d\n");
+
+    return 0;
+}
