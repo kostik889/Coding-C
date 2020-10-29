d4f1e91dacb90b07aee848694466b1590faf562b
Matěj Kostrhoun <matej@LAPTOP-IS5INFVB.KOSTRHOUN.local>
2020-10-29 01:08:52 +0100

    kódování cp 852

---
+++ test_diakritiky.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    printf("V pı¡livu §luœouŸkìch kvØt… vŸelky se vzn çej¡.\n");
+    printf("HleÔ, toœ Ÿarovnì je lou§ek, kde hedv bn‚ çtØst¡Ÿka z ı¡\n");
+    printf("Vodn¡ §¡åky bØ§¡ kolem lesn¡ t…nØ a kadeıemi svìmi Ÿeı¡ stı¡brosvit mØs¡ce.\n");
+    printf("Qv¡do, kouzeln¡k…v uŸeå s Ôol¡Ÿky ut¡r  prach z v¡l¡ch kı¡del.\n");
+    printf("à, n hlì £svit obla§il zemØtv ı prol¡naj¡c¡m hıejivìm dotekem svìm.");
+
+    return 0;
+}
