9b7f8e83e61afc2127801c0e6b1f091d6a4daf06
Matěj Kostrhoun <matej@LAPTOP-IS5INFVB.KOSTRHOUN.local>
2020-12-22 23:22:52 +0100

    w3resource-basic

--- W3resource/C Basic/12.c
+++ W3resource/C Basic/12.c
@@ -4,7 +4,7 @@ int main(){
     printf("Write a C program that accepts an employee's ID, total worked hours of a month and the amount he received per hour. Print the employee's ID and salary (with two decimal places) of a particular month.");
 
     printf("\nTest Data :\n");
-    printf("Input the Employees ID(Max. 10 chars):");
+    printf("Input the Employees ID(Max. 10 chars): ");
 
     char ID[10];
     fgets(ID, 10, stdin);
@@ -12,10 +12,22 @@ int main(){
     printf("Input the working hrs: ");
     unsigned int working_hours=0;
     if(scanf("%u", &working_hours) ==1){
-        printf("this is good");
+        printf("Salary amount/hr: ");
+        unsigned int salary_amount_hr=0;
+        if(scanf("%u", &salary_amount_hr) ==1){
+            printf("---------------------------------------------");
+            printf("\nEmployees ID = %s", ID);
+            printf("Salary = U$ %u", working_hours*salary_amount_hr);
+            printf("\n---------------------------------------------");
+        }else
+        {
+            printf("Salary amount/hr have to be unsigned int!!!\n");
+        }
+        
+
     }else
     {
-        printf("this is bad\n");
+        printf("You have to use unsigned int!!!\n");
     }
     
 
