e1e80e63c838ec6946a8ff9abb7f7ef36426dd96
Matěj Kostrhoun <matej@LAPTOP-IS5INFVB.KOSTRHOUN.local>
2020-12-22 19:49:28 +0100

    name, date of birth, mobile printer

---
+++ W3resource/C Basic/1.c
@@ -0,0 +1,6 @@
+/*Write a C program to print your name, date of birth. and mobile number. */
+int main(){
+    printf("Name    : Matej Kostrhoun\n");
+    printf("DOB     : 22. April 1995\n");
+    printf("Mobile  : 123-456-789\n");
+}
